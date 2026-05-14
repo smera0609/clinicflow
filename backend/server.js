eec96136733ecd3361c9dc4884dc3afe8848914d
Smera S Kini <smeraskini@gmail.com>
2026-05-14 17:12:51 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.get("/", async (req, res) => {
 app.use("/api/contact", contactRoutes);
 app.use("/api/appointments", appointmentRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
